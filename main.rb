0d93716512b7a66daca5f4527be9390e4218db07
Mateus Vieira <mateus.vieira@sidedoor.com.br>
2024-01-25 10:04:33 -0300

    Adicionando

--- main.rb
+++ main.rb
@@ -1,22 +1,22 @@
 def substring(phrase)
-  dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit","teste"]
-  new_hash=Hash.new
-  if phrase.include?(' ')
-    fulltext = phrase.split(' ')
-  else
-    fulltext=[phrase]
-  end
+  dictionary = %w[below down go going horn how howdy it i low own part partner
+                  sit teste]
+  new_hash = {}
+  fulltext = if phrase.include?(' ')
+               phrase.split(' ')
+             else
+               [phrase]
+             end
   fulltext.each do |word|
+    next unless dictionary.include?(word)
 
-    if dictionary.include?(word)
-      #new_hash[word]||=0
-      new_hash[word]+=1
-      puts new_hash
-    end
+    # new_hash[word]||=0
+    new_hash[word] += 1
+    puts new_hash
   end
 end
-puts "Type a sentence"
+puts 'Type a sentence'
 phrase = gets.chomp
 
-
 substring(phrase)
+puts 'teste'
